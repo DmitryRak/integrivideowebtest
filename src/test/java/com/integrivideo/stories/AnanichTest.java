24a234d025bfa1fbba6ef6bd9fef347608c7a2a2
Pavel Ananich <pavel.ananich@outlook.com>
2017-09-02 20:23:40 +0300

    * New branch pavel_tests * new story AnanichTest

---
+++ src/test/java/com/integrivideo/stories/AnanichTest.java
@@ -0,0 +1,40 @@
+package com.integrivideo.stories;
+
+import com.integrivideo.Data;
+import com.integrivideo.steps.CommonSteps;
+import com.integrivideo.steps.LandingPageSteps;
+import com.integrivideo.steps.LoginSteps;
+import com.integrivideo.steps.SignUpSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Dmitry Rak on 7/12/2017.
+ */
+@Story(AnanichTest.class)
+@RunWith(SerenityRunner.class)
+public class AnanichTest extends BaseTest {
+
+    @Steps
+    CommonSteps commonSteps;
+    @Steps
+    LandingPageSteps landingPageSteps;
+    @Steps
+    LoginSteps loginSteps;
+    @Steps
+    SignUpSteps signUpSteps;
+
+    @Test
+    public void newUserCanBeRegistered() {
+        landingPageSteps.goToSignUpLinkFromFirstBlock();
+        signUpSteps.enterCredentialsAndSignUp(Data.RANDOM_EMAIL, Data.USER_1_PASSWORD);
+        commonSteps.notificationMessageShouldBeLike("Message with instructions was sent");
+        loginSteps.shouldBeOnLoginPage();
+    }
+
+    //TODO add test to validate email
+    //TODO add test to validate activation
+}
