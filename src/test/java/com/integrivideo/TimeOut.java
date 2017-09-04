d5a1bd30b004fd24da648ce1b23b3a7539189d4d
Dmitry Rak <dmitryrak11@gmail.com>
2017-09-04 22:21:06 +0300

    updated versions and work with timeout

---
+++ src/test/java/com/integrivideo/TimeOut.java
@@ -0,0 +1,7 @@
+package com.integrivideo;
+
+public class TimeOut {
+    public final static int SMALL_TIMEOUT = 1000;
+    public final static int MEDIUM_TIMEOUT = 5000;
+    public final static int LONG_TIMEOUT = 10000;
+}
